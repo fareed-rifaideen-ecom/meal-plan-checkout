24d6d34b7b85d7194c31643851d6d022a07ebf49
Fareed M. Rifaideen <fareed.rifaideen@gmail.com>
2026-06-12 11:03:10 +0400

    Change sidebar menu title from 'Meal Plan Cleanup' to 'Plan Cleanup'

--- meal-plan-checkout.php
+++ meal-plan-checkout.php
@@ -1046,7 +1046,7 @@ function cmp_add_cleanup_tool_menu() {
     // 1. Create the Top-Level Menu "Meal Portal"
     add_menu_page(
         'Meal Portal Settings',       // Page title
-        'Meal Plan Cleanup',          // Menu title on the sidebar
+        'Plan Cleanup',               // Menu title on the sidebar
         'manage_options',             // Capability required (Admins only)
         'meal-portal-main',           // Main Menu slug
         'cmp_render_cleanup_page',    // Function to render the page
